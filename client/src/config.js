be97700a2edefb3f96518cc9abbbeec152b35db9
Thiekshana Soysa <actmasters@gmail.com>
2025-02-08 13:09:52 +0800

    vercel updates

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,2 @@
-export const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+export const API_BASE_URL = "https://api.feargamingproductions.com/api";
 export const API_URL = `${API_BASE_URL}`;
